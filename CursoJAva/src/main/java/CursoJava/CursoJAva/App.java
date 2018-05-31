45f78756b4731191f87a155be2b9161afaf3ad8f
CMRG <39797586+CMRG@users.noreply.github.com>
2018-05-31 13:30:31 +0200

    Cris primer commit

---
+++ CursoJAva/src/main/java/CursoJava/CursoJAva/App.java
@@ -0,0 +1,13 @@
+package CursoJava.CursoJAva;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
