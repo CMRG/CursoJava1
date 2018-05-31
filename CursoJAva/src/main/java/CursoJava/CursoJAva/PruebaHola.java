13e83a9f7f54ebc9b924a4b6c2375909cb77d776
CMRG <39797586+CMRG@users.noreply.github.com>
2018-05-31 13:55:57 +0200

    Cris 2º

---
+++ CursoJAva/src/main/java/CursoJava/CursoJAva/PruebaHola.java
@@ -0,0 +1,11 @@
+package CursoJava.CursoJAva;
+
+public class PruebaHola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println( "Hola mundo" );
+
+	}
+
+}
